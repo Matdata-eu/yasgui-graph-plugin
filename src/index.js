27bcb65f4fbd309a1a69cd21431d8643799116d1
Mathias Vanden Auweele <mathias@matdata.eu>
2025-12-06 12:15:09 +0100

    fix: label should be upper case

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import GraphPlugin from './GraphPlugin.js';
 
 // Auto-register plugin if Yasgui is available (UMD global)
 if (typeof window !== 'undefined' && window.Yasgui && window.Yasgui.Yasr) {
-  window.Yasgui.Yasr.registerPlugin('graph', GraphPlugin);
+  window.Yasgui.Yasr.registerPlugin('Graph', GraphPlugin);
 }
 
 export default GraphPlugin;
